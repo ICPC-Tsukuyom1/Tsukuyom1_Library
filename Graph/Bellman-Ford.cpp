012cb3b58a46b6d03655427cd7df874b39fc5fe2
jupiter_68 <takanori31045@gmail.com>
2025-05-14 15:54:50 +0900

    Update Bellman-Ford.cpp
    return vector<ll> -> return bool

--- Graph/Bellman-Ford.cpp
+++ Graph/Bellman-Ford.cpp
@@ -1,15 +1,16 @@
-vector<ll> bellman_ford(Edges &edges, ll V, ll s) {
-    vector<ll> dist(V, INF);
+bool bellman_ford(Edges &edges, vector<ll> &dist ll V, ll s) {
+    dist = vector<ll>(V, INF);
     dist[s] = 0;
-    for (ll i = 0; i < V - 1; i++) {
+    for (ll i = 0; i < V; i++) {
+        if(i == V-1) return false;
+        bool load = false;
         for (auto e : edges) {
             if (dist[e.from] == INF) continue;
-            dist[e.to] = min(dist[e.to], dist[e.from] + e.cost);
+            if(chmin(dist[e.to], dist[e.from] + e.cost)){
+                 load = true;
+            }
         }
+        if(!load) break;
     }
-    for (auto e : edges) {
-        if (dist[e.from] == INF) continue;
-        if (dist[e.from] + e.cost < dist[e.to]) return vector<ll>();
-    }
-    return dist;
+    return true;
 }

bbf23b399f174f1c6aebfa81d75edc3a82cf4300
oxojo <oxojo9x@gmail.com>
2025-05-14 16:43:59 +0900

    convex_hull 微修正

--- Geometry/convex_hull.cpp
+++ Geometry/convex_hull.cpp
@@ -9,7 +9,7 @@ Polygon convex_hull(Polygon S) {
         qs[k++] = S[i];
     }
     for (ll i = S.size() - 2, t = k; i >= 0; i--) {
-        while (k > t && cross(qs[k - 1] - qs[k - 2], S[i] - qs[k - 1]) < -EPS) k--;
+        while (k > t && cross(qs[k - 1] - qs[k - 2], S[i] - qs[k - 1]) <= -EPS) k--;
         qs[k++] = S[i];
     }
     qs.resize(k - 1);

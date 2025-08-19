cafdb114a042c8a41d0a3194aacc47446f346f81
Oxojo <oxojo9x@gmail.com>
2025-08-19 22:51:03 +0900

    fix: area.cpp を短縮

--- Geometry/area.cpp
+++ Geometry/area.cpp
@@ -1,13 +1,9 @@
+// need: Point.cpp
 ld Area(Polygon S) {
-    if (S.size() < 3) return 0;
     ld ans = 0;
-    Point O = S[0];
-    for (ll i = 1; i + 1 < S.size(); i++) {
-        Point A = S[i], B = S[i+1];
-        Vector C = A-O;
-        Vector D = B-O;
-        ld area = cross(C, D) * 0.5;
-        ans += area;
+    rep(i, sz(S) - 2) {
+        Point a = S[i + 1] - S[0], b = S[i + 2] - S[0];
+        ans += a.cross(b) * 0.5;
     }
     return ans;
 }

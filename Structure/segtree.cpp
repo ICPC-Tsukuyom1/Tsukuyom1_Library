dc4d8d7391498bd863d887a6e81a9973376bcf6d
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-08-19 11:17:32 +0000

    説明をソースコードに移動

--- Structure/segtree.cpp
+++ Structure/segtree.cpp
@@ -7,10 +7,15 @@ const ll INF = 4e18;
 struct SegTree {
   int n = 1;
   vector<T> v;
+
+  // 構築 O(N log N)
   SegTree(int N) {
     while(n < N) n *= 2;
     v.assign(n * 2, e);
   }
+
+  // 一点更新 O(log N)
+  // i : [0, n)
   void update(int i, T a) {
     i += n;
     v[i] = a;
@@ -19,17 +24,21 @@ struct SegTree {
       v[i] = op(v[i * 2], v[i * 2 + 1]);
     }
   }
-  T _query(int l, int r, int x, int lx, int rx) {
+
+  T _(int l, int r, int x, int lx, int rx) {
     if(rx <= l || r <= lx) return e;
-    if(lx >= l && rx <= r) return v[x];
+    if(l <= lx && rx <= r) return v[x];
     int m = (lx + rx) / 2;
     return op(
-      _query(l, r, x * 2, lx, m),
-      _query(l, r, x * 2 + 1, m, rx)
+      _(l, r, x * 2, lx, m),
+      _(l, r, x * 2 + 1, m, rx)
     );
   }
+  
+  // 区間総積取得 O(log N)
+  // l : [0, n), r : [1, n], l < r
   T query(int l, int r) {
-    return _query(l, r, 1, 0, n);
+    return _(l, r, 1, 0, n);
   }
 };
 
--- Structure/segtree.typ
+++ Structure/segtree.typ
@@ -3,14 +3,7 @@
 == SegTree
 
 セグメント木。演算および型、単位元はdefineを書き換えてください。
-0-indexed, [l, r)の半開区間で使えるようになっています。
-
-*使い方*
-
-- `void update(int i, T a)` : i番目(0-indexed) の値を書き換え $O(log N)$
-- `T query(int l, int r)` : 半開区間[l, r)の区間全体への演算結果を得る $O(log N)$
 
 *実装*
 
 #sourcefile(read("../Structure/segtree.cpp"), lang: "cpp")
-

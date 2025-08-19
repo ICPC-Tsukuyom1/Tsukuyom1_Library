b4fb09c6c7de5d5078902fc254bda7e2ce06ad07
Oxojo <oxojo9x@gmail.com>
2025-08-19 23:14:28 +0900

    fix: area ゴルフ

--- Geometry/area.cpp
+++ Geometry/area.cpp
@@ -2,8 +2,7 @@
 ld Area(Polygon S) {
     ld ans = 0;
     rep(i, sz(S) - 2) {
-        Point a = S[i + 1] - S[0], b = S[i + 2] - S[0];
-        ans += a.cross(b) * 0.5;
+        ans += (S[i + 1] - S[0]).cross(S[i + 2] - S[0]) * 0.5;
     }
     return ans;
 }

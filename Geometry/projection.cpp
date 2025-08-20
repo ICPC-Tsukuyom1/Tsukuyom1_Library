5cece6b9727aa65cead52729930fb5712466802c
Oxojo <oxojo9x@gmail.com>
2025-08-21 08:42:01 +0900

    fix: segment の点の命名変更に伴う修正

--- Geometry/projection.cpp
+++ Geometry/projection.cpp
@@ -1,8 +1,8 @@
 Point Projection(Segment S, Point p, bool reflect) {
-    Vector base = S.B - S.A;
-    ld r = (p - S.A).dot(base) / base.norm();
+    Vector base = S.e - S.s;
+    ld r = (p - S.s).dot(base) / base.norm();
     if (reflect) {
-        return p + (S.A + base * r - p) * 2.0;
+        return p + (S.s + base * r - p) * 2.0;
     }
-    else return S.A + base * r;
+    else return S.s + base * r;
 }

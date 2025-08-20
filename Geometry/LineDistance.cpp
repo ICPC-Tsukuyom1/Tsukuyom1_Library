583ffb76887e8e69be08cdce290848787a64edda
Oxojo <oxojo9x@gmail.com>
2025-08-21 08:42:24 +0900

    feat: lineDistance の作成

--- Geometry/LineDistance.cpp
+++ Geometry/LineDistance.cpp
@@ -1,4 +1,5 @@
 // need: Point.cpp
+// dist between a *segment* and a point -> segDistance.cpp
 ld lineDist(Line L, Point p) {
-    return (L.B - L.A).cross(p - L.A)/(L.B - L.A).abs();
+    return (L.e - L.s).cross(p - L.s)/(L.e - L.s).abs();
 }
---
+++ Geometry/lineDistance.typ
@@ -0,0 +1,5 @@
+#import "@preview/codelst:2.0.2": sourcefile, sourcecode
+
+== lineDistance
+直線と点の距離を求める。
+#sourcefile(read("../Geometry/lineDistance.cpp"), lang: "cpp")

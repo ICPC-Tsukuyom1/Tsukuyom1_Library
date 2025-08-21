8fe38af00a425ade1b61b455a67bd5fa057f6dc1
Oxojo <oxojo9x@gmail.com>
2025-08-21 09:23:13 +0900

    feat: segDistance 追加

---
+++ Geometry/segDistance.cpp
@@ -0,0 +1,7 @@
+// need: Point.cpp
+// dist between a *line* and a point -> lineDistance.cpp
+ld segDist(Segment S, Point p) {
+	if (S.A == S.B) return (p-S.A).abs();
+	auto d = (S.B - S.A).norm(), t = min(d, max(.0, (p - S.A).dot(S.B - S.A));
+	return ((p - S.A) * d - (S.B - S.A) * t).abs() / d;
+}
---
+++ Geometry/segDistance.typ
@@ -0,0 +1,5 @@
+#import "@preview/codelst:2.0.2": sourcefile, sourcecode
+
+== segDistance
+線分と点の距離を求める。
+#sourcefile(read("../Geometry/segDistance.cpp"), lang: "cpp")

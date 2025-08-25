e32e6c7b82aa8b2a16d7258fcbd115fa16a988fc
Oxojo <oxojo9x@gmail.com>
2025-08-26 07:53:17 +0900

    update: template 追加

--- template.cpp
+++ template.cpp
@@ -4,6 +4,11 @@ using ll = long long;
 using ld = long double;
 using pll = pair<ll, ll>;
 using vl = vector<ll>;
+#define v(type) vector<type>
+#define vv(type) vector<vector<type>>
+#define vvv(type) vector<vector<vector<type>>>
+template <class T> using minpq = priority_queue<T, vector<T>, greater<T>>;
+template <class T> using maxpq = priority_queue<T>;
 #define rep(i, r) for(ll i = 0; i < (r); i++)
 #define reps(i, l, r) for(ll i = (l); i < (r); i++)
 #define rrep(i, r, l) for(ll i = (r) - 1; i >= l; i--)

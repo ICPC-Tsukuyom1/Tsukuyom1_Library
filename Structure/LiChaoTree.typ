bebbf1c981137a099467149df8e3ede9b294aa28
Oxojo <oxojo9x@gmail.com>
2025-08-26 09:16:53 +0900

    feat: Li-Chao Tree 追加

---
+++ Structure/LiChaoTree.typ
@@ -0,0 +1,7 @@
+#import "@preview/codelst:2.0.2": sourcefile, sourcecode
+
+== Li-Chao Tree
+
+直線・線分 $a x+b$ の追加と点 $x$ での最小値クエリを $O(log{N})$ で処理する。
+
+#sourcefile(read("../Structure/LiChaoTree.cpp"), lang: "cpp")
---
+++ verify/yosupo/segment_add_get_min.cpp
@@ -0,0 +1,54 @@
+//@yosupo segment_add_get_min
+#include "../../template.cpp"
+#include "../../Structure/LiChaoTree.cpp"
+struct query {
+	ll t;
+	ll l, r, a, b;
+};
+void solve() {
+	ll N, Q; cin >> N >> Q;
+	vl x;
+	ll Max = 1000000007;
+	v(query) q;
+	rep(i, N) {
+		ll l, r, a, b;
+		cin >> l >> r >> a >> b;
+		q.emplace_back(0, l, r, a, b);
+	}
+	while (Q--) {
+		ll t; cin >> t;
+		if (t == 0) {
+			ll l, r, a, b;
+			cin >> l >> r >> a >> b;
+			q.emplace_back(t, l, r, a, b);
+		}
+		else {
+			ll p; cin >> p;
+			q.emplace_back(t, p, 0, 0, 0);
+			x.emplace_back(p);
+		}
+	}
+	if (x.empty()) {
+		return;
+	}
+	LCTree lct(x);
+	rep(i, sz(q)) {
+		if (q[i].t == 0) {
+			lct.update_seg(q[i].l, q[i].r, q[i].a, q[i].b);
+		}
+		else {
+			ll ans = lct.query(q[i].l);
+			if (ans == INF) {
+				cout << "INFINITY" << endl;
+			}
+			else cout << ans << endl;
+		}
+	}
+}
+int main() {
+	ll T = 1;
+	// cin >> T;
+	while (T--) {
+		solve();
+	}
+}

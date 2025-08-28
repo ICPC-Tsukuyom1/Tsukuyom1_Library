6410ecf36bd95ce45bf0a60b20af0ed5eb3c4e98
Oxojo <oxojo9x@gmail.com>
2025-08-28 23:44:30 +0900

    verify 追加

---
+++ verify/yosupo/math/inverse_matrix.cpp
@@ -0,0 +1,22 @@
+//@yosupo inverse_matrix
+
+#include "template.cpp"
+const ll mod = 998244353;
+#include "Math/modint.cpp"
+#include "Math/matrix.cpp"
+
+int main() {
+    ll N; cin >> N;
+	Matrix<mint> a(N);
+	rep(i, N) rep(j, N) cin >> a[i][j];
+	if (a.det() == mint(0)) {
+		cout << -1 << endl;
+		return;
+	}
+	Matrix<mint> b = a.inv();
+	rep(i, N) {
+		rep(j, N) {
+			cout << b[i][j] << " \n"[j == N - 1];
+		}
+	}
+}

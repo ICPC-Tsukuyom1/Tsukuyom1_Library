825bd2e93e8468365bc81e4f10c768b5a0a1f236
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-08-28 12:36:11 +0000

    Revert "DFSを非再帰で実装"
    This reverts commit ebd3b3d60548cded642a0befbbdf7d1ff3191240.

--- Graph/rerooting.cpp
+++ Graph/rerooting.cpp
@@ -4,49 +4,27 @@ v(V) rerooting(Graph<T>& G)
   ll n = G.size();
   v(V) ans0(n);
   vector<v(E)> l_acc(n), r_acc(n);
-
-  stack<tuple<ll, ll, bool>> dfs0;
-  dfs0.push({0, -1, true});
-  while (!dfs0.empty())
+  auto dfs0 = [&](auto&& f, ll i, ll p) -> void
   {
-    auto [i, p, first] = dfs0.top();
-    dfs0.pop();
-    if (first)
+    v(E) cache;
+    for (auto& c : G[i])
     {
-      dfs0.push({i, p, false});
-      for (auto& c : G[i])
-      {
-        if (c.to == p) continue;
-        dfs0.push({c.to, i, true});
-      }
+      if (c.to == p) continue;
+      f(f, c.to, i);
+      cache.push_back(put_edge(ans0[c.to], c));
     }
-    else
-    {
-      v(E) cache;
-      for (auto& c : G[i])
-      {
-        if (c.to == p) continue;
-        cache.push_back(put_edge(ans0[c.to], c));
-      }
-      l_acc[i].push_back(ee);
-      r_acc[i].push_back(ee);
-      rep(j, cache.size()) l_acc[i].push_back(op(l_acc[i].back(), cache[j]));
-      rrep(j, 0, cache.size()) r_acc[i].push_back(op(r_acc[i].back(), cache[j]));
-      reverse(all(r_acc[i]));
-      ans0[i] = put_vert(r_acc[i][0], i);
-    }
-  }
-
+    l_acc[i].push_back(ee);
+    r_acc[i].push_back(ee);
+    rep(j, cache.size()) l_acc[i].push_back(op(l_acc[i].back(), cache[j]));
+    rrep(j, 0, cache.size()) r_acc[i].push_back(op(r_acc[i].back(), cache[j]));
+    reverse(all(r_acc[i]));
+    ans0[i] = put_vert(r_acc[i][0], i);
+  };
+  dfs0(dfs0, 0, -1);
   v(V) ans(n);
   ans[0] = ans0[0];
-
-  stack<tuple<ll, ll, E>> dfs;
-  dfs.push({0, -1, ee});
-  while (!dfs.empty())
+  auto dfs = [&](auto&& f, ll i, ll p, E rev) -> void
   {
-    auto [i, p, rev] = dfs.top();
-    dfs.pop();
-
     ans[i] = put_vert(op(r_acc[i][0], rev), i);
     auto l = l_acc[i].begin(), r = r_acc[i].begin();
     for (auto& c : G[i])
@@ -57,8 +35,9 @@ v(V) rerooting(Graph<T>& G)
       ++l;
       auto merged = op(rev, bottom);
       auto verted = put_vert(merged, i);
-      dfs.push({c.to, i, put_edge(verted, c)});
+      f(f, c.to, i, put_edge(verted, c));
     }
-  }
+  };
+  dfs(dfs, 0, -1, ee);
   return ans;
 }

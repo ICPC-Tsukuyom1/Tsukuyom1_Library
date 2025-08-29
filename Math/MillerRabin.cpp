06619889b1a69aaeb2e39ac6bde97cdfb7d82728
Oxojo <oxojo9x@gmail.com>
2025-08-30 08:16:45 +0900

    fixed millerrabin

--- Math/MillerRabin.cpp
+++ Math/MillerRabin.cpp
@@ -13,7 +13,7 @@ bool isprime(ll n) {
     ll a[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022}, s = __builtin_ctzll(n - 1), d = n >> s;
     for (ll m : a) {
         ll p = modpow(m % n, d, n), i = s;
-        while (p != 1 && p != n - 1 && a % n && i--) p = (p * p) % n;
+        while (p != 1 && p != n - 1 && m % n && i--) p = (p * p) % n;
         if (p != n - 1 && i != s) return 0;
     }
     return 1;

88f18b5fcb8eee05f6623408ba9e6376a73db8ae
Oxojo <oxojo9x@gmail.com>
2025-08-30 16:06:23 +0900

    euler_phi 修正

--- Math/Euler_phi.cpp
+++ Math/Euler_phi.cpp
@@ -2,11 +2,11 @@
 ll Euler_phi(ll N) {
     vl A = fac(N); sort(all(A));
     A.erase(unique(all(A)), A.end());
-    for (ll i = 0; i < A.size(); i++) {
-        N /= A[i].first;
+    for (ll i = 0; i < sz(A); i++) {
+        N /= A[i];
     }
-    for (ll i = 0; i < A.size(); i++) {
-        N *= (A[i].first - 1);
+    for (ll i = 0; i < sz(A); i++) {
+        N *= (A[i] - 1);
     }
     return N;
 }

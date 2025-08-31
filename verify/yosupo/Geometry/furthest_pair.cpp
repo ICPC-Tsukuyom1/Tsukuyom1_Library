43a9dc26c557c61946dca30d25485d3c7577682d
Oxojo <oxojo9x@gmail.com>
2025-09-01 08:34:32 +0900

    Geometry verify 追加

---
+++ verify/yosupo/Geometry/furthest_pair.cpp
@@ -0,0 +1,21 @@
+//@yosupo furthest_pair
+
+#include "../../../template.cpp"
+#include "../../../Geometry/Point.cpp"
+#include "../../../Geometry/convex_hull.cpp"
+#include "../../../Geometry/diamConvex.cpp"
+void solve() {
+	ll n; cin >> n;
+	Polygon g(n);
+	rep(i, n) cin >> g[i].x >> g[i].y;
+	auto res = diamConvex(g);
+	ll l, r;
+	rep(i, n) if (res.first == g[i]) { l = i; break; }
+	rep(i, n) if (res.second == g[i] && l != i) { r = i; break; }
+	cout << l << ' ' << r << endl;
+}
+int main() {
+	ll T = 1;
+	cin >> T;
+	while (T--) solve();
+}

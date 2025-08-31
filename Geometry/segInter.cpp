e5c04c205d50a22fac5cefabbabce9dae1a9d8c8
Oxojo <oxojo9x@gmail.com>
2025-08-31 18:24:57 +0900

    segInter 追加

---
+++ Geometry/segInter.cpp
@@ -0,0 +1,19 @@
+#include "Point.cpp"
+#include "onSegment.cpp"
+vec<Point> segInter(Point a, Point b, Point c, Point d) {
+	ld oa = c.cross(d, a), ob = c.cross(d, b), oc = a.cross(b, c), od = a.cross(b, d);
+	if (sign(oa) * sign(ob) < EPS && sign(oc) * sign(od) < EPS) {
+		return { (a * ob - b * oa) / (ob - oa) };
+	}
+	set<Point> s;
+	if (onSegment({c, d}, a)) s.insert(a);
+	if (onSegment({c, d}, b)) s.insert(b);
+	if (onSegment({a, b}, c)) s.insert(c);
+	if (onSegment({a, b}, d)) s.insert(d);
+	return {all(s)};
+}
+vec<Point> segInter(Segment a, Segment b) {
+	return segInter(a.s, a.e, b.s, b.e);
+}
+
+// check sz(segInter) > 0

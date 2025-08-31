143eac3a11c81f781a983de09c7d946bb9ea3b2c
Oxojo <oxojo9x@gmail.com>
2025-08-31 18:23:00 +0900

    Segment 命名変更に伴う修正

--- Geometry/segDistance.cpp
+++ Geometry/segDistance.cpp
@@ -1,7 +1,7 @@
 #include "./Point.cpp"
 // dist between a *line* and a point -> lineDistance.cpp
 ld segDist(Segment S, Point p) {
-	if (S.A == S.B) return (p-S.A).abs();
-	auto d = (S.B - S.A).norm(), t = min(d, max(.0, (p - S.A).dot(S.B - S.A));
-	return ((p - S.A) * d - (S.B - S.A) * t).abs() / d;
+	if (S.s == S.e) return (p-S.s).abs();
+	auto d = (S.e - S.s).norm(), t = min(d, max(.0, (p - S.s).dot(S.e - S.s)));
+	return ((p - S.s) * d - (S.e - S.s) * t).abs() / d;
 }

8d13ed3af1a7c32ca3990c8175cc6511e0d1f987
Oxojo <oxojo9x@gmail.com>
2025-09-01 11:23:36 +0900

    commonTangents が同じ線を含まないように

--- Geometry/commonTangents.cpp
+++ Geometry/commonTangents.cpp
@@ -7,22 +7,22 @@ vec<Line> commonTangents(Circle c1, Circle c2) {
 	}
 	typedef Point P;
 	ld d = (c1.c - c2.c).abs();
-	vec<Line> L;
+	set<pair<Point, Point>> L;
 	if (c1.r - c2.r <= d + EPS) {
 		if (c1.r - c2.r <= EPS) {
 			P D = (c2.c - c1.c).perp() / d * c1.r;
-			L.push_back(Line(c1.c + D, c2.c + D));
-			L.push_back(Line(c1.c - D, c2.c - D));
+			L.insert({c1.c + D, c2.c + D});
+			L.insert({c1.c - D, c2.c - D});
 		} else {
 			ld m = (c1.r - c2.r) * (c1.r - c2.r) / d;
 			P M = c1.c + (c2.c - c1.c) / d * m;
 			ld h = sqrtl((c1.r - c2.r) * (c1.r - c2.r) - m * m);
 			P H1 = M + (c2.c - c1.c).normal() * h;
 			P D1 = (H1 - c1.c).unit() * c2.r;
-			L.push_back(Line(H1 + D1, c2.c + D1));
+			L.insert({H1 + D1, c2.c + D1});
 			P H2 = M - (c2.c - c1.c).normal() * h;
 			P D2 = (H2 - c1.c).unit() * c2.r;
-			L.push_back(Line(H2 + D2, c2.c + D2));
+			L.insert({H2 + D2, c2.c + D2});
 		}
 	}
 	if (c1.r + c2.r <= d + EPS) {
@@ -31,13 +31,16 @@ vec<Line> commonTangents(Circle c1, Circle c2) {
 		ld h = sqrtl((c1.r + c2.r) * (c1.r + c2.r) - m * m);
 		P H1 = M + (c2.c - c1.c).normal() * h;
 		P D1 = (H1 - c1.c).unit() * c2.r;
-		L.push_back(Line(H1 - D1, c2.c - D1));
+		L.insert({H1 - D1, c2.c - D1});
 		P H2 = M - (c2.c - c1.c).normal() * h;
 		P D2 = (H2 - c1.c).unit() * c2.r;
-		L.push_back(Line(H2 - D2, c2.c - D2));
+		L.insert({H2 - D2, c2.c - D2});
 	}
-	if (s) {
-		rep(i, sz(L)) swap(L[i].s, L[i].e);
+	vec<Line> ans;
+	while (!L.empty()) {
+		auto it = *L.begin(); L.erase(L.begin());
+		if (s) swap(it.first, it.second);
+		ans.push_back(Line(it.first, it.second));
 	}
-	return L;
+	return ans;
 }

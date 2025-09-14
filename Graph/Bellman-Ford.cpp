eae63493aa1d938c78e876d746387d8362c3b585
Oxojo <oxojo9x@gmail.com>
2025-09-14 22:22:13 +0900

    fix: bellman-ford, bfs, dfs

--- Graph/Bellman-Ford.cpp
+++ Graph/Bellman-Ford.cpp
@@ -1,6 +1,8 @@
-//need: Graph.cpp
-bool bellman_ford(Edges &edges, vector<ll> &dist, ll V, ll s) {
-    dist = vector<ll>(V, INF);
+struct Edge {
+    ll from, to, cost;
+};
+bool bellman_ford(vec<Edge> &edges, vl &dist, ll V, ll s) {
+    dist = vl(V, INF);
     dist[s] = 0;
     rep(i, V - 1) {
         for (auto e : edges) {
--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -1,10 +1,9 @@
-//need: Graph.cpp
-void dfs(Graph &G, vector<ll>&dist, int v) {
-    for (Edge e : G[v]) {
-        if (dist[e.to] != INF) {
+void dfs(vv<pll> &G, vl &dist, int v) {
+    for (pll e : G[v]) {
+        if (dist[e.first] != INF) {
             continue;
         }
-        dist[e.to] = dist[v] + e.cost;
-        dfs(G, dist, e.to);
+        dist[e.first] = dist[v] + e.second;
+        dfs(G, dist, e.first);
     }
 }

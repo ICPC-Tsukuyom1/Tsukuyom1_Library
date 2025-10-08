16457dfb273e2da303bea3e4e617ad469a54d792
Oxojo <oxojo9x@gmail.com>
2025-10-09 07:46:57 +0900

    feat: add verify for bipartite matching and general matching

---
+++ verify/yosupo/Graph/general_matching.cpp
@@ -0,0 +1,24 @@
+//@yosupo general_matching
+
+#include "../../../template.cpp"
+#include "../../../Graph/maxMatching.cpp"
+void solve() {
+	ll n, m; cin >> n >> m;
+	vv<ll> g(n);
+	rep(i, m) {
+		ll u, v; cin >> u >> v;
+		g[u].emplace_back(v);
+		g[v].emplace_back(u);
+	}
+	maxMatch a(g);
+	auto ans = a.max_match();
+	cout << sz(ans) << endl;
+	rep(i, sz(ans)) {
+		cout << ans[i] << endl;
+	}
+}
+int main() {
+	ll T = 1;
+	// cin >> T;
+	while (T--) solve();
+}

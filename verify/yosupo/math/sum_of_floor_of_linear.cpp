e408f18a6579dc1c71ad90b92fd0980c717b0aa5
oxojo <oxojo9x@gmail.com>
2025-10-29 15:15:48 +0900

    add floor_sum (+ verify)

---
+++ verify/yosupo/math/sum_of_floor_of_linear.cpp
@@ -0,0 +1,14 @@
+//@yosupo sum_of_floor_of_linear
+
+#include "../../../template.cpp"
+#include "../../../Math/floorsum.cpp"
+
+void solve() {
+    ll n, m, a, b; cin >> n >> m >> a >> b;
+	cout << floor_sum(n, a, b, m) << endl;
+}
+int main() {
+    ll T = 1;
+    cin >> T;
+    while (T--) solve();
+}

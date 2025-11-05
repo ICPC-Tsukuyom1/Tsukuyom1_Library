16b71b4b0bb655db6ab0b8e5b4b04908c7e8c810
oxojo <oxojo9x@gmail.com>
2025-11-05 20:32:03 +0900

    fix scc & other related files

--- Graph/SCC.cpp
+++ Graph/SCC.cpp
@@ -1,50 +1,50 @@
-// need: Graph.cp
 struct SCC {
-  Graph &g;
-  Graph gg, rg;
-  vector<ll> cmp, ord, use;
+  vv<ll>& g;
+  vv<ll> dag, rg;
+  vl cmp, ord;
+  vec<bool> use;
+  vv<ll> blng;
 
-  SCC(Graph &g)
-      : g(g), gg(g.size()), rg(g.size()), cmp(g.size(), -1), use(g.size()) {
-    rep(i, g.size()) {
-      for (auto e : g[i]) {
-        gg.add(i, e.to, 1, 1);
-        rg.add(e.to, i, 1, 1);
-      }
-    }
-  }
+  SCC(vv<ll>& g) : g(g), use(sz(g), 0) { build(); }
 
   ll operator[](ll k) { return cmp[k]; }
+
+  vl& belong(ll i) { return blng[i]; }
 
   void dfs(ll p) {
     if (use[p]) return;
     use[p] = 1;
-    for (auto e : gg[p]) dfs(e.to);
+    for (auto e : g[p]) dfs(e);
     ord.push_back(p);
   }
 
   void rdfs(ll p, ll cnt) {
     if (cmp[p] != -1) return;
     cmp[p] = cnt;
-    for (auto e : rg[p]) rdfs(e.to, cnt);
+    for (auto e : rg[p]) rdfs(e, cnt);
   }
 
-  Graph build() {  // build SCC
-    rep(i, gg.size()) { dfs(i); }
+  void build() {
+    rep(i, sz(g)) { dfs(i); }
     reverse(all(ord));
-    ll ptr = 0;
-    for (ll i : ord) {
-      if (cmp[i] == -1) rdfs(i, ptr), ptr++;
+    use.clear(), use.shrink_to_fit();
+    cmp.resize(sz(g), -1ll), rg.resize(sz(g));
+    rep(i, sz(g)) {
+      for (auto e : g[i]) { rg[e].emplace_back(i); }
     }
-
-    Graph t(ptr);
-    rep(i, g.size()) {
-      for (auto &e : g[i]) {
-        ll x = cmp[i], y = cmp[e.to];
+    ll ptr = 0;
+    for (ll i : ord)
+      if (cmp[i] == -1) rdfs(i, ptr), ptr++;
+    rg.clear(), rg.shrink_to_fit();
+    ord.clear(), ord.shrink_to_fit();
+    dag.resize(ptr), blng.resize(ptr);
+    rep(i, sz(g)) {
+      blng[cmp[i]].push_back(i);
+      for (auto& to : g[i]) {
+        ll x = cmp[i], y = cmp[to];
         if (x == y) continue;
-        t.add(x, y, 1, 1);
+        dag[x].push_back(y);
       }
     }
-    return t;
   }
 };

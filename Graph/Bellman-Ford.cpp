d61354020f27e03fd0eb7309e935f49120cc5c0b
oxojo <oxojo9x@gmail.com>
2025-11-05 20:42:03 +0900

    fix verify

--- Graph/Bellman-Ford.cpp
+++ Graph/Bellman-Ford.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 struct Edge {
     ll from, to, cost;
 };
@@ -11,13 +10,6 @@ bool bellman_ford(vec<Edge> &edges, vl &dist, ll V, ll s) {
             chmin(dist[e.to], dist[e.from] + e.cost);
         }
     }
-=======
-// need: Graph.cpp
-bool bellman_ford(Edges &edges, vector<ll> &dist, ll V, ll s) {
-  dist = vector<ll>(V, INF);
-  dist[s] = 0;
-  rep(i, V - 1) {
->>>>>>> main
     for (auto e : edges) {
       if (dist[e.from] == INF) continue;
       chmin(dist[e.to], dist[e.from] + e.cost);

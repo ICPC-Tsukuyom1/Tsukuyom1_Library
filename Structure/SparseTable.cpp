e32b7d227ceb1481d6f5066793d212a66d0f3503
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-11-06 13:49:11 +0000

    SparceTableを短くした・Verify追加

--- Structure/SparseTable.cpp
+++ Structure/SparseTable.cpp
@@ -1,32 +1,22 @@
-template <typename T = ll, typename F = ll>
 struct SparseTable {
-  F f;
-  vector<vector<T>> st;
-  vector<ll> lookup;
+  vv<T> st;
+  vl lookup;
 
-  SparseTable() = default;
-
-  explicit SparseTable(const vector<T> &v, const F &f) : f(f) {
-    const ll N = (ll)v.size();
-    ll siz = 0;
-    ll tmp = N;
-    while (tmp != 0) {
-      siz++;
-      tmp >>= 1;
-    }
-    st.assign(siz, vector<T>(N));
-    rep(i, v.size()) { st[0][i] = v[i]; }
+  SparseTable(const vec<T> &v) {
+    ll siz = bit_width((unsigned)sz(v));
+    st.assign(siz, vec<T>(sz(v)));
+    st[0] = v;
     reps(i, 1, siz) {
-      rep(j, N + 1 - (1 << i)) {
-        st[i][j] = f(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+      rep(j, sz(v) + 1 - (1 << i)) {
+        st[i][j] = op(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
       }
     }
-    lookup.resize(v.size() + 1);
-    reps(i, 2, lookup.size()) { lookup[i] = lookup[i >> 1] + 1; }
+    lookup.resize(sz(v) + 1);
+    reps(i, 2, sz(lookup)) { lookup[i] = lookup[i >> 1] + 1; }
   }
 
-  inline T fold(ll l, ll r) const {
+  T fold(ll l, ll r) {
     ll b = lookup[r - l];
-    return f(st[b][l], st[b][r - (1 << b)]);
+    return op(st[b][l], st[b][r - (1 << b)]);
   }
 };

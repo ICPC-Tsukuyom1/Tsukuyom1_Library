e75fedf69e3da31e2480caba71d726381130911e
oxojo <oxojo9x@gmail.com>
2025-11-11 15:59:35 +0900

    golf seginter

--- Geometry/segInter.cpp
+++ Geometry/segInter.cpp
@@ -1,11 +1,10 @@
 // depends on: Point, onSegment
 vec<Point> segInter(Point a, Point b, Point c, Point d) {
-  ld oa = c.cross(d, a), ob = c.cross(d, b), oc = a.cross(b, c),
-     od = a.cross(b, d);
-  if (sign(oa) * sign(ob) < 0 && sign(oc) * sign(od) < 0) {
-    return {(a * ob - b * oa) / (ob - oa)};
-  }
-  set<Point> s;
+  ld oa=c.cross(d,a),ob=c.cross(d,b)
+  ld oc=a.cross(b,c),od=a.cross(b,d);
+  if (sign(oa)*sign(ob)<0 && sign(oc)*sign(od)<0)
+    return {(a*ob-b*oa)/(ob-oa)};
+  set<Point>s;
   if (onSegment({c, d}, a)) s.insert(a);
   if (onSegment({c, d}, b)) s.insert(b);
   if (onSegment({a, b}, c)) s.insert(c);

5e376a210b5144ef509fdb769dbd6f9cc7d17df2
oxojo <oxojo9x@gmail.com>
2025-11-11 14:40:47 +0900

    golf convex_hull

--- Geometry/convex_hull.cpp
+++ Geometry/convex_hull.cpp
@@ -1,26 +1,20 @@
 // depends on: Point
 // strict = true => NOT include points on edge
 // strict = false => include points on edge
-Polygon convex_hull(Polygon &p, bool strict = true) {
+Polygon convex_hull(Polygon &p, bool str = 1) {
   sort(all(p));
   p.erase(unique(all(p)), p.end());
   ll n = sz(p), k = 0;
   if (n <= 2) return p;
-  vec<Point> ch(2 * n);
+  vec<Point> c(2*n);
   auto check = [&](ll i) {
-    Point a = ch[k - 1] - ch[k - 2], b = p[i] - ch[k - 1];
-    if (strict) {
-      return a.cross(b) <= EPS;
-    } else {
-      return a.cross(b) <= -EPS;
-    }
+    Point a = c[k-1] - c[k-2], b = p[i] - c[k-1];
+    return a.cross(b) <= (strict ? EPS : -EPS);
   };
-  for (ll i = 0; i < n; ch[k++] = p[i++]) {
+  for (ll i = 0; i < n; c[k++] = p[i++])
     while (k >= 2 && check(i)) --k;
-  }
-  for (ll i = n - 2, t = k + 1; i >= 0; ch[k++] = p[i--]) {
+  for (ll i = n-2, t = k+1; i; c[k++] = p[i--])
     while (k >= t && check(i)) --k;
-  }
-  ch.resize(k - 1);
-  return ch;
+  c.resize(k-1);
+  return c;
 }

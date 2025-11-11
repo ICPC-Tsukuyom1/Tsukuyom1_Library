d44209397589666dbcbc13f39c650deb77277e81
oxojo <oxojo9x@gmail.com>
2025-11-11 14:55:46 +0900

    fix code

--- Geometry/isconvex.cpp
+++ Geometry/isconvex.cpp
@@ -3,6 +3,6 @@ bool isconvex(Polygon S) {
   ll n = sz(S);
   bool c = true;
   rep(i, n)
-    c &= (ccw(S[(i+n-1) % n],S[i],S[(i+1) % n]) != -1);
+    c&=(ccw(S[(i+n-1)%n],S[i],S[(i+1)%n]) != -1);
   return c;
 }
